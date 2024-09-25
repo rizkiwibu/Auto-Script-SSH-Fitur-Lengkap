f41119585a0f038353a88ab6fedeeb8ed9460209
DEFF <89836212+DEFF-OFFICIAL@users.noreply.github.com>
2024-09-26 02:57:20 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -31,7 +31,7 @@ clear
 ipsaya=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/Rizalasg/install/main/ip"
+data_ip="https://raw.githubusercontent.com/DEFF-OFFICIAL/install/main/ip"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
